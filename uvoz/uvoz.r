ccf9c54906316aa75ec7967b7afa34c1fdc468fd
Jaka mrak <mrak.jaka9@gmail.com>
2020-12-28 18:33:26 +0100

    napisal poročilo za prvo fazo

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,55 +1,4 @@
 ## 2. faza: Uvoz podatkov
-#
-sl <- locale("sl", decimal_mark=",", grouping_mark=".")
-
-# Funkcija, ki uvozi občine iz Wikipedije
-uvozi.obcine <- function() {
-  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-  stran <- html_session(link) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec=",")
-  for (i in 1:ncol(tabela)) {
-    if (is.character(tabela[[i]])) {
-      Encoding(tabela[[i]]) <- "UTF-8"
-    }
-  }
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    if (is.character(tabela[[col]])) {
-      tabela[[col]] <- parse_number(tabela[[col]], na="-", locale=sl)
-    }
-  }
-  for (col in c("obcina", "pokrajina", "regija")) {
-    tabela[[col]] <- factor(tabela[[col]])
-  }
-  return(tabela)
-}
-
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function(obcine) {
-  data <- read_csv2("podatki/druzine.csv", col_names=c("obcina", 1:4),
-                    locale=locale(encoding="Windows-1250"))
-  data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-    strapplyc("([^ ]+)") %>% sapply(paste, collapse=" ") %>% unlist()
-  data$obcina[data$obcina == "Sveti Jurij"] <- iconv("Sveti Jurij ob Ščavnici", to="UTF-8")
-  data <- data %>% pivot_longer(`1`:`4`, names_to="velikost.druzine", values_to="stevilo.druzin")
-  data$velikost.druzine <- parse_number(data$velikost.druzine)
-  data$obcina <- parse_factor(data$obcina, levels=obcine)
-  return(data)
-}
- #Zapišimo podatke v razpredelnico obcine
-obcine <- uvozi.obcine()
- #Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine(levels(obcine$obcina))
- #Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
- #potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
 
 
 library(rjson)
@@ -120,10 +69,11 @@ indeks.cen.arh.proj.uvoz <- read_delim("podatki/arh-proj.csv", ";",
 
 indeks.cen.arh.proj <- indeks.cen.arh.proj.uvoz %>% 
   rename(INDEKS=3, SKD=`SKD DEJAVNOST`) %>%   # poenostavitev imen
-  separate(ČETRTLETJE, c("LETO", "CETR"), "Q") %>% # razbitje na dva stolpca
-  group_by(LETO, SKD) %>% # grupiranje po letih, SKD damo samo še zraven, ker je itak isti
-  summarise(POV_INDEKS=mean(INDEKS)) %>%
+  separate(ČETRTLETJE, c("Leto", "CETR"), "Q") %>% # razbitje na dva stolpca
+  group_by(Leto, SKD) %>% # grupiranje po letih, SKD damo samo še zraven, ker je itak isti
+  summarise(PovprecenIndeks=mean(INDEKS)) %>%
   select(-2) #odstranimo stolpec skd saj nam ne rabi
+  
 
 
 #INDEKS POVPREČNE MESEČNE NETO PLAČE PO REGIJAH
@@ -162,4 +112,4 @@ json.podatki <- fromJSON(file = json.file)
 vrednost.gradb.del <- sapply(json.podatki$data, unlist) %>% #poberemo vn podatke
   t() %>% .[, c("key3", "values")] %>% #transponiramo in vzamemo stolpca z letom in vrednostjo
   data.frame(stringsAsFactors=FALSE) %>%   #spremenimo v data frame
-  transmute(Leto=parse_number(key3), Vrednost_1000MioEUR=parse_number(values))  #preimenujemo in nastavimo tip
+  transmute(Leto=parse_number(key3), Vrednost_1000EUR=parse_number(values))  #preimenujemo in nastavimo tip
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,15 +1,15 @@
 # 3. faza: Vizualizacija podatkov
+library(ggplot2)
 
-# Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
-                             pot.zemljevida="OB", encoding="Windows-1250")
-# Če zemljevid nima nastavljene projekcije, jo ročno določimo
-proj4string(zemljevid) <- CRS("+proj=utm +zone=10+datum=WGS84")
+source('lib/libraries.r', encoding = 'UTF-8')
 
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
 
-# Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+#testni graf
+graf1 <- vrednost.gradb.del %>% 
+  ggplot(aes(x=leto, y=vrednost)) + 
+  geom_line(size=1, colour="green") + 
+  geom_point(size=2, colour="blue") +
+  xlab("leto") +
+  ylab("vrednost") +
+  labs(title="Vrednost gradbenih del po letih ") 
+
