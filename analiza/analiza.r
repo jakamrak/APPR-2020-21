c590949d98dbf8c03c8bf3b70023ef3ea60e2628
Jaka mrak <mrak.jaka9@gmail.com>
2021-01-04 17:29:57 +0100

    popravil regresijo in poročilo

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -3,18 +3,18 @@
 podatki <- gradbeni.stroski %>% filter(TipStroska=="StroskiMateriala") %>%
   select(-"TipStroska")
 
+prileganje <- glm(data = podatki, Indeks ~ Leto)
 
-prileganje <- lm(data = podatki, Indeks ~ Leto)
+leta <- data.frame(Leto=seq(2016, 2020, 1))
+napoved <- mutate(leta, Indeks=predict(prileganje, leta))
 
-l <- data.frame(Leto=seq(2016, 2020, 1))
-napoved <- mutate(l, Indeks=predict(prileganje, l))
+podatki <- podatki %>% rbind(napoved %>% filter(Leto==2020)) #na stare podatke dodamo leto 2020 iz napovedi
 
-
-graf_regresija <- ggplot(podatki, aes(x=Leto, y=Indeks)) + 
+graf_regresija <- podatki %>% ggplot(aes(x=Leto, y=Indeks)) + 
   geom_point(size=3, color="red") + 
-  geom_smooth(method='lm', formula=y ~ poly(x,2,raw=TRUE), fullrange=TRUE, color='darkblue') +
+  geom_smooth(method='glm', formula=y ~ poly(x,2,raw=TRUE), fullrange=TRUE, color='darkblue') +
   scale_x_continuous('Leto', breaks = seq(2016, 2020, 1), limits = c(2016,2020)) +
   ylab("Indeks stroškov materiala") +
   labs(title = "Napoved indeksa stroškov materiala za leto 2020")
-  
+plot(graf_regresija)  
 
